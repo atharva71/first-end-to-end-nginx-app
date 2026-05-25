cf31b5e17883a4f13d70ac35896c47aba0e7245c
Ubuntu <pardeshiatharva71@gmail.com>
2026-05-25 17:57:43 +0000

    Modified

--- backend-api/app.js
+++ backend-api/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+const redis = require('redis');
 const app = express();
 app.use(cors());
+
+const client = redis.createClient({
+    url: 'redis://redis-service:6379'
+});
+
+client.connect();
+
+client.on('error', (err) => {
+    console.log('Redis Error:', err);
+});
 
 const PORT = 3000;
 
@@ -16,8 +27,10 @@ app.get('/health', (req, res) => {
     });
 });
 
+
 app.get('/products', (req, res) => {
-    res.json([
+
+    const products = [
         {
             id: 1,
             name: 'Laptop',
@@ -28,8 +41,38 @@ app.get('/products', (req, res) => {
             name: 'Phone',
             price: 20000
         }
-    ]);
+    ];
+
+    res.send(products);
 });
+
+app.post('/cart', async (req, res) => {
+
+    const product = req.query.product;
+
+    let cart = await client.get('cart');
+
+    cart = cart ? JSON.parse(cart) : [];
+
+    cart.push(product);
+
+    await client.set('cart', JSON.stringify(cart));
+
+    res.send({
+        message: 'Product added to cart',
+        cart
+    });
+});
+
+app.get('/cart', async (req, res) => {
+
+    let cart = await client.get('cart');
+
+    cart = cart ? JSON.parse(cart) : [];
+
+    res.send(cart);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
